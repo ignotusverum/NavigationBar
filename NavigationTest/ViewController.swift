5cbf06eb2b3a2c39ad1858d744944a8fdea62312
Vlad Z <vladislavz1991@gmail.com>
2016-09-06 22:21:08 -0400

    initial

---
+++ NavigationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationTest
+//
+//  Created by Vladislav Zagorodnyuk on 9/6/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
